6234a18a7aa123d08458d92d648ddf293616f4c2
Xzayler <csak.mityu@gmail.com>
2024-08-15 21:15:50 +0200

    chore: Remove unecessary template files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,16 @@
-import { createAsync, type RouteDefinition } from "@solidjs/router";
-import { getUser, logout } from "~/lib";
+// import { createAsync, type RouteDefinition } from '@solidjs/router';
 
-export const route = {
-  preload() { getUser() }
-} satisfies RouteDefinition;
+// export const route = {
+//   preload() {
+//     getUser();
+//   },
+// } satisfies RouteDefinition;
 
 export default function Home() {
-  const user = createAsync(() => getUser(), { deferStream: true });
+  // const user = createAsync(() => getUser(), { deferStream: true });
   return (
-    <main class="w-full p-4 space-y-2">
-      <h2 class="font-bold text-3xl">Hello {user()?.username}</h2>
-      <h3 class="font-bold text-xl">Message board</h3>
-      <form action={logout} method="post">
-        <button name="logout" type="submit">
-          Logout
-        </button>
-      </form>
+    <main class="w-full min-h-full">
+      <h1>TIC-TAC-TOE</h1>
     </main>
   );
 }
