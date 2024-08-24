7e057f6189fe412bccdb39f510e47d15add04b63
Xzayler <csak.mityu@gmail.com>
2024-08-24 09:23:22 +0200

    feat: Add link to git repo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,10 @@
-// import { createAsync, type RouteDefinition } from '@solidjs/router';
 import { A } from '@solidjs/router';
-
-// export const route = {
-//   preload() {
-//     getUser();
-//   },
-// } satisfies RouteDefinition;
+import GithubIcon from '~/components/icons/GithubIcon';
 
 export default function Home() {
-  // const user = createAsync(() => getUser(), { deferStream: true });
   return (
-    <main class="grow w-full min-h-full flex flex-col justify-center items-center gap-8">
-      <div>
+    <main class="grow w-full min-h-full flex flex-col justify-center items-center">
+      <div class="mb-8">
         <h1 class="text-4xl text-center">
           Tic-Tac-Toe
           <br />
@@ -21,9 +14,21 @@ export default function Home() {
 
       <A
         href="./game"
-        class="text-center px-2 py-1 border-4 border-foreground text-foreground rounded-xl hover:bg-foreground hover:text-background transition hover:scale-105"
+        class="mb-2 text-center px-2 py-1 border-4 border-foreground text-foreground rounded-xl hover:bg-foreground hover:text-background transition hover:scale-105"
       >
         Start Game
+      </A>
+      <A
+        href="https://github.com/Xzayler/Tic-Tac-Toe-Infinity"
+        target="_blank"
+        class="text-center px-2 py-1 border-4 border-foreground text-foreground rounded-xl hover:bg-foreground hover:text-background transition"
+      >
+        <div class="flex max-w-max max-h-6 gap-1">
+          <div class="aspect-square w-6">
+            <GithubIcon />
+          </div>
+          <div>Check the Repo</div>
+        </div>
       </A>
     </main>
   );
